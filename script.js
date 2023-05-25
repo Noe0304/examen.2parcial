3be772a6dee74406c7b71e5b1af324f2801c2c02
Noe0304 <132527739+Noe0304@users.noreply.github.com>
2023-05-24 22:04:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+let uno = true;
+let dos = "2";
+let tres = 3;
+let cuatro = null;
+let cinco;
+let seis = {numero: 6};
+let siete = [7];
+
+console.log(typeof true);
+console.log(typeof "2");
+console.log(typeof 3);
+console.log(typeof null);
+console.log(typeof {numero: 6});
+console.log(typeof cinco);
+console.log(typeof [7]); 
+
+
+let producto = 'soda'
+let precio = '15'
+let costoEnvio = '150'
+let dirrecionEnvio = 'Bolivia' 
+let pais = 'EEUU'
+
+console.log(producto, precio, costoEnvio, dirrecionEnvio, pais); 
+
+let libreria = "React es una biblioteca Javascript de código abierto creada por Facebook" // 7
+
+let nombre = "libreia";   // 8
+console.log(nombre.length);
+
+
+let string = "TOYOTA, SUZUKI, BMW, FORD, KIA, ISUZU, AUDI"; // 11
+console.log(string.split(" ",));
+
+let letra = "R"
+console.error(letra.charCodeAt()); 
+
+let string1 = " Hola mundo "; // 13 
+console.log(string1.repeat(4));
+
+
+let random = Math.random();
+   let numero = random * 25 
+   let valorfinal = numero + 50 
+   console.log(Math.floor(valorfinal));
+
+
+let num = 9.8;
+let numInt = parseInt(num);
+console.log(numInt); // 18
+console.log(Math.abs(-5));
+console.log();
+console.log();
+console.log();
+
+
+console.log(Math.min(4, 8, 16, 18, -52, -3, -1, 7)); // 19
+console.log(Math.max(4, 8, 16, 18, -52, -3, -1, 7)); 
+
+
+console.log(Math.floor(9.78)); //  20
+console.log(Math.floor(5.20));
+console.log(Math.floor(1.64));
+console.log(Math.floor(5.7));
+console.log(Math.floor(3.23));
+
+console.log(Math.ceil(9.78));
+console.log(Math.ceil(5.20));
+console.log(Math.ceil(1.64));
+console.log(Math.ceil(5.7));
+console.log(Math.ceil(3.23));
+
